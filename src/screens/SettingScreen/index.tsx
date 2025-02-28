986c6b61aa4f066e95322db9a7649e8660cb4d88
Rohit Chahal <rohitchahal@Rohits-Mac-mini.local>
2025-03-01 00:33:59 +0530

    upgrade to 0.77.1

--- src/screens/SettingScreen/index.tsx
+++ src/screens/SettingScreen/index.tsx
@@ -1,45 +1,94 @@
-import React from 'react'
-import { StyleSheet, ScrollView } from 'react-native'
-import { View, Text, Colors, Assets } from 'react-native-ui-lib'
-import { useNavigation } from '@react-navigation/native'
-import NavBar from 'components/NavBar'
-import SettingItem from './SettingItem'
-import Routes from 'config/Routes'
+import React from 'react';
+import {StyleSheet, ScrollView} from 'react-native';
+import {View, Text, Colors, Assets} from 'react-native-ui-lib';
+import {useNavigation} from '@react-navigation/native';
+import NavBar from 'components/NavBar';
+import SettingItem from './SettingItem';
+import Routes from 'config/Routes';
 
 const SettingScreen = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    const onGoBack = () => navigation.goBack();
-    const onNavigateUpdateProfile = () => navigation.navigate(Routes.UpdateProfile);
+  const onGoBack = () => navigation.goBack();
+  const onNavigateUpdateProfile = () =>
+    navigation.navigate(Routes.UpdateProfile);
 
-    return (
-        <View flex>
-            <NavBar title='Settings' leftIcon='back' onLeftClick={onGoBack} />
-            <ScrollView style={{ flex: 1, backgroundColor: Colors.white }} showsVerticalScrollIndicator={false}>
-                <View flex backgroundColor={Colors.cf7} marginB-60>
-                    <View marginB-small>
-                        <SettingItem onPress={onNavigateUpdateProfile} icon={Assets.icons.ic_update_profile} label='Update Profile' />
-                        <SettingItem onPress={() => { }} icon={Assets.icons.ic_notification} label='Notification Settings' />
-                        <SettingItem onPress={() => { }} icon={Assets.icons.ic_article_comment} label='Send Feedback' />
-                        <SettingItem onPress={() => { }} icon={Assets.icons.ic_city_info} label='How is work?' />
-                        <SettingItem onChangeSwitch={() => { }} icon={Assets.icons.ic_auto_trip} label='Auto create trip' isSwitch={true} />
-                        <SettingItem onChangeSwitch={() => { }} icon={Assets.icons.ic_setting_faceid} label='Quick login use FaceID' isSwitch={true} />
-                        <SettingItem onPress={() => { }} icon={Assets.icons.ic_clear_history} label='Clear search history' isArrow={false} />
-                    </View>
-                    <View>
-                        <SettingItem onPress={() => { }} icon={Assets.icons.ic_term} label='Terms of Service' />
-                        <SettingItem onPress={() => { }} icon={Assets.icons.ic_policy} label='Privacy Policy' />
-                        <SettingItem onPress={() => { }} icon={Assets.icons.ic_update} label='Check For Update' isArrow={false} />
-                    </View>
-                </View>
-                <View flex paddingB-small backgroundColor={Colors.white}>
-                    <Text M14 c83 center>Triplan version 2.0.0.4</Text>
-                </View>
-            </ScrollView>
+  return (
+    <View flex>
+      <NavBar title="Settings" leftIcon="back" onLeftClick={onGoBack} />
+      <ScrollView
+        style={{flex: 1, backgroundColor: Colors.white}}
+        showsVerticalScrollIndicator={false}>
+        <View flex backgroundColor={Colors.cf7} marginB-60>
+          <View marginB-small>
+            <SettingItem
+              onPress={onNavigateUpdateProfile}
+              icon={Assets.icons.ic_update_profile}
+              label="Update Profile"
+            />
+            <SettingItem
+              onPress={() => {}}
+              icon={Assets.icons.ic_notification}
+              label="Notification Settings"
+            />
+            <SettingItem
+              onPress={() => {}}
+              icon={Assets.icons.ic_article_comment}
+              label="Send Feedback"
+            />
+            <SettingItem
+              onPress={() => {}}
+              icon={Assets.icons.ic_city_info}
+              label="How is work?"
+            />
+            <SettingItem
+              onChangeSwitch={() => {}}
+              icon={Assets.icons.ic_auto_trip}
+              label="Auto create trip"
+              isSwitch={true}
+            />
+            <SettingItem
+              onChangeSwitch={() => {}}
+              icon={Assets.icons.ic_setting_faceid}
+              label="Quick login use FaceID"
+              isSwitch={true}
+            />
+            <SettingItem
+              onPress={() => {}}
+              icon={Assets.icons.ic_clear_history}
+              label="Clear search history"
+              isArrow={false}
+            />
+          </View>
+          <View>
+            <SettingItem
+              onPress={() => {}}
+              icon={Assets.icons.ic_term}
+              label="Terms of Service"
+            />
+            <SettingItem
+              onPress={() => {}}
+              icon={Assets.icons.ic_policy}
+              label="Privacy Policy"
+            />
+            <SettingItem
+              onPress={() => {}}
+              icon={Assets.icons.ic_update}
+              label="Check For Update"
+              isArrow={false}
+            />
+          </View>
         </View>
-    )
-}
+        <View flex paddingB-small backgroundColor={Colors.white}>
+          <Text M14 c83 center>
+            triplan version 2.0.0.4
+          </Text>
+        </View>
+      </ScrollView>
+    </View>
+  );
+};
 
-export default SettingScreen
+export default SettingScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/SignUpScreen/index.tsx
+++ src/screens/SignUpScreen/index.tsx
@@ -1,98 +1,141 @@
-import React, { useState } from 'react'
-import { StyleSheet, StatusBar, TouchableOpacity } from 'react-native'
-import { View, Text, KeyboardAwareScrollView, Image, Assets, Colors } from 'react-native-ui-lib';
-import { useNavigation } from '@react-navigation/native';
+import React, {useState} from 'react';
+import {StyleSheet, StatusBar, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  KeyboardAwareScrollView,
+  Image,
+  Assets,
+  Colors,
+} from 'react-native-ui-lib';
+import {useNavigation} from '@react-navigation/native';
 import NavBar from 'components/NavBar';
-import { width, height, heightHeader } from 'config/scaleAccordingToDevice';
+import {width, height, heightHeader} from 'config/scaleAccordingToDevice';
 import InputText from 'components/InputText';
 import BaseButton from 'components/BaseButton';
 
 interface SocialButtonProps {
-    socialIcon: any,
-    onPress: () => void,
-    color: any
+  socialIcon: any;
+  onPress: () => void;
+  color: any;
 }
 
-const SocialButton: React.FC<SocialButtonProps> = ({ socialIcon, onPress, color }) => {
-    return (
-        <TouchableOpacity onPress={onPress}>
-            <View center backgroundColor={color} height={50} width={width * 0.4} br100>
-                <Image source={socialIcon} />
-            </View>
-        </TouchableOpacity>
-    )
-}
+const SocialButton: React.FC<SocialButtonProps> = ({
+  socialIcon,
+  onPress,
+  color,
+}) => {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View
+        center
+        backgroundColor={color}
+        height={50}
+        width={width * 0.4}
+        br100>
+        <Image source={socialIcon} />
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const SignUpScreen = () => {
-    const navigation = useNavigation();
-    const [fullName, setFullName] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
+  const navigation = useNavigation();
+  const [fullName, setFullName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
 
-    const goBack = () => navigation.goBack();
+  const goBack = () => navigation.goBack();
 
-    return (
-
-        <>
-            <StatusBar barStyle='dark-content' translucent backgroundColor="transparent" />
-            <NavBar leftIcon='back' onLeftClick={goBack} />
-            <KeyboardAwareScrollView
-                showsVerticalScrollIndicator={false}
-                keyboardDismissMode="interactive"
-            >
-                <View paddingH-medium height={height - heightHeader} backgroundColor={Colors.white}>
-                    <View marginB-10>
-                        <Text c35 B40 marginB-32>Sign Up</Text>
-                        <Text c59 M16>{`Sign up with Facebook or Google+ to sync \nyour travel photo.`}</Text>
-                    </View>
-                    <View row spread marginB-medium>
-                        <SocialButton socialIcon={Assets.icons.ic_facebook} color={Colors.c3b} onPress={() => { }} />
-                        <SocialButton socialIcon={Assets.icons.ic_gplus} color={Colors.cdd4} onPress={() => { }} />
-                    </View>
-                    <Text c59 M14 marginB-medium>Or using your email</Text>
-                    <View>
-                        <InputText
-                            value={fullName}
-                            onChange={(text) => setFullName(text)}
-                            placeholder='Full Name'
-                            iconLeft={Assets.icons.ic_user_16}
-                            style={{ marginBottom: 16 }}
-                            keyboardType='email-address'
-                        />
-                        <InputText
-                            value={email}
-                            onChange={(text) => setEmail(text)}
-                            placeholder='Email'
-                            iconLeft={Assets.icons.ic_email_16}
-                            style={{ marginBottom: 16 }}
-                            keyboardType='email-address'
-                        />
-                        <InputText
-                            value={password}
-                            onChange={(text) => setPassword(text)}
-                            placeholder='Password'
-                            iconLeft={Assets.icons.ic_password}
-                            iconRight={Assets.icons.ic_show_pass}
-                            secureTextEntry={!isShowPassword}
-                            onIcRightClick={() => setIsShowPassword(!isShowPassword)}
-                            style={{ marginBottom: 32 }}
-                        />
-                        <BaseButton primary label='Sign Up' onPress={() => { }} />
-                    </View>
-                    <View flex bottom paddingB-42>
-                        <Text c83 M12 center>{`By tapping any of the button above, I agree to Triplan’s \n`}
-                            <Text c83 M12 style={{ textDecorationLine: 'underline' }}>Term of Service</Text>
-                            <Text c83 M12> and </Text>
-                            <Text c83 M12 style={{ textDecorationLine: 'underline' }}>Privacy Policy</Text>
-                        </Text>
-                    </View>
-                </View>
-            </KeyboardAwareScrollView>
-        </>
-    )
-}
+  return (
+    <>
+      <StatusBar
+        barStyle="dark-content"
+        translucent
+        backgroundColor="transparent"
+      />
+      <NavBar leftIcon="back" onLeftClick={goBack} />
+      <KeyboardAwareScrollView
+        showsVerticalScrollIndicator={false}
+        keyboardDismissMode="interactive">
+        <View
+          paddingH-medium
+          height={height - heightHeader}
+          backgroundColor={Colors.white}>
+          <View marginB-10>
+            <Text c35 B40 marginB-32>
+              Sign Up
+            </Text>
+            <Text
+              c59
+              M16>{`Sign up with Facebook or Google+ to sync \nyour travel photo.`}</Text>
+          </View>
+          <View row spread marginB-medium>
+            <SocialButton
+              socialIcon={Assets.icons.ic_facebook}
+              color={Colors.c3b}
+              onPress={() => {}}
+            />
+            <SocialButton
+              socialIcon={Assets.icons.ic_gplus}
+              color={Colors.cdd4}
+              onPress={() => {}}
+            />
+          </View>
+          <Text c59 M14 marginB-medium>
+            Or using your email
+          </Text>
+          <View>
+            <InputText
+              value={fullName}
+              onChange={text => setFullName(text)}
+              placeholder="Full Name"
+              iconLeft={Assets.icons.ic_user_16}
+              style={{marginBottom: 16}}
+              keyboardType="email-address"
+            />
+            <InputText
+              value={email}
+              onChange={text => setEmail(text)}
+              placeholder="Email"
+              iconLeft={Assets.icons.ic_email_16}
+              style={{marginBottom: 16}}
+              keyboardType="email-address"
+            />
+            <InputText
+              value={password}
+              onChange={text => setPassword(text)}
+              placeholder="Password"
+              iconLeft={Assets.icons.ic_password}
+              iconRight={Assets.icons.ic_show_pass}
+              secureTextEntry={!isShowPassword}
+              onIcRightClick={() => setIsShowPassword(!isShowPassword)}
+              style={{marginBottom: 32}}
+            />
+            <BaseButton primary label="Sign Up" onPress={() => {}} />
+          </View>
+          <View flex bottom paddingB-42>
+            <Text c83 M12 center>
+              {`By tapping any of the button above, I agree to triplan’s \n`}
+              <Text c83 M12 style={{textDecorationLine: 'underline'}}>
+                Term of Service
+              </Text>
+              <Text c83 M12>
+                {' '}
+                and{' '}
+              </Text>
+              <Text c83 M12 style={{textDecorationLine: 'underline'}}>
+                Privacy Policy
+              </Text>
+            </Text>
+          </View>
+        </View>
+      </KeyboardAwareScrollView>
+    </>
+  );
+};
 
 export default SignUpScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
